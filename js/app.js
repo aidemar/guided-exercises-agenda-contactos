45fbfeea16285cb352a402f32cd57dff6b41a0e5
AnettTruMa <anettp.trujillo@gmail.com>
2018-01-18 10:17:51 -0600

    add function

--- js/app.js
+++ js/app.js
@@ -1,48 +1,60 @@
 
   //Traer elementos del HTML
-  
-  
-  var loadPage = function () {
-    
+  var $nameInput = $('#name-input');
+  var $phoneInput = $('#phone-input');
+
+  function loadPage () {
+  $(".modal").modal();
+  $nameInput.keyup(validateContact);
+  $phoneInput.keyup(validateContact);
+  };
+
+  function validateContact() {
+    var $containerAddContact = $('#add-contact')
+    if($(this).val().trim().length > 0 ){
+      $containerAddContact.removeAttr('disabled');
+    } else {
+      $containerAddContact.attr('disabled', true);
+    }
   };
 
   var paintContactsInHTML = function(contact) {
-   
+
     /* Crear elementos con DOM html al publicar contacto */
-     
-    
+
     //Asignando atributos y/o eventos
-    
+
 
     /* Asignando valores a los elementos*/
-    
+
 
     //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
+
   };
 
   var addContact = function (e) {
     e.preventDefault();
     //Las siguientes lineas toman el valor que el usuario agrega en los inputs y los guardan en variables
-    
+
 
     //Con las variables separadas que obtuvimos creamos una estructura de datos unica, un objeto por cada contacto
-    
+
 
     /*Agregamos el contacto a nuestra data (arreglo declarado) para poderla filtrar y eliminar posteriormente*/
-    
+
     //La siguiente funcion se encarga de pintar los contactos en el html
-    
+
     /* Limpiando valores de formulario*/
-    
+
   };
 
-  
 
 
-  var validateContact = function () {
+
+  //var validateContact = function () {
     //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
-  };
+
+
 
 
   var filterContacts = function() {
